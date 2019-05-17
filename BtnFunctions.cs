108fba7dcb13e877a01d1458617e79334b78b4d9
Kristianto <50426071+kristiantoerger@users.noreply.github.com>
2019-05-18 02:31:37 +0700

    SCRIPTS

---
+++ BtnFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FungsiTombol : MonoBehaviour {
+    public GameObject panelmainMenu;
+    public GameObject panelCara;
+
+    public void PindahScene(string namascene)
+    {
+        SceneManager.LoadScene(namascene);
+    }
+
+    public void BukaMainMenu()
+    {
+        panelmainMenu.SetActive(true);
+        panelCara.SetActive(false);
+    }
+    public void BukaCara()
+    {
+        panelmainMenu.SetActive(false);
+        panelCara.SetActive(true);
+    }
+    public void BukaBendera()
+    {
+        panelmainMenu.SetActive(false);
+        panelCara.SetActive(false);
+    }
+}
